0303c91e18d80b8845a72fedafc997e09f45e46d
Bolek <103572991+BoloMasta@users.noreply.github.com>
2023-02-09 17:47:39 +0100

    useSelector remove

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { fetchContacts } from 'redux/contacts/operations';
 import { ContactForm } from '../components/ContactForm/ContactForm';
 import { StatusBar } from '../components/StatusBar/StatusBar';
