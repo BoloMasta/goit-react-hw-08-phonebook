ae260044b368c87a1ed0f7d2aa92451ea9cd42ca
Bolek <103572991+BoloMasta@users.noreply.github.com>
2023-02-09 21:48:31 +0100

    starting material

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,11 +1,20 @@
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
+import Button from '@mui/material/Button';
+//import DeleteIcon from '@mui/icons-material/Delete';
 import { deleteContact } from '../../redux/contacts/operations';
 import { selectIsLoading } from 'redux/contacts/selectors';
 import css from './ContactListItem.module.scss';
 import userIcon from '../../images/user.png';
 import telephoneIcon from '../../images/telephone.png';
+
+import Typography from '@mui/material/Typography';
+
+import ListItem from '@mui/material/ListItem';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CallIcon from '@mui/icons-material/Call';
 
 export const ContactListItem = ({ contact }) => {
   const dispatch = useDispatch();
@@ -20,7 +29,7 @@ export const ContactListItem = ({ contact }) => {
   // };
 
   return (
-    <li className={clsx(css.item, isLoading && css.loading)}>
+    <ListItem className={clsx(css.item, isLoading && css.loading)}>
       <div className={css.item_icons}>
         <img src={userIcon} alt="user icon" className={css.user_icon} />
         {/* <button
@@ -29,7 +38,13 @@ export const ContactListItem = ({ contact }) => {
           onClick={addLike}
         ></button> */}
       </div>
-      <p className={css.text}>
+      <Typography variant="h6">
+        {contact.name}: {contact.number}
+      </Typography>
+      <IconButton href={`tel:${contact.number}`}>
+        <CallIcon />
+      </IconButton>
+      {/* <p className={css.text}>
         {contact.name}: {contact.number}
         <a href={`tel:${contact.number}`}>
           <img
@@ -39,11 +54,15 @@ export const ContactListItem = ({ contact }) => {
             className={css.telephone_icon}
           />
         </a>
-      </p>
-      <button className={css.button} type="button" onClick={handleDelete}>
+      </p> */}
+      {/* <button className={css.button} type="button" onClick={handleDelete}>
         Delete
-      </button>
-    </li>
+      </button> */}
+
+      <IconButton onClick={handleDelete} sx={{ marginLeft: 'auto' }}>
+        <DeleteIcon />
+      </IconButton>
+    </ListItem>
   );
 };
 
