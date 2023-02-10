244fdfd42b8a0ad2103b37e33fa34da8f2578c2d
Bolek <103572991+BoloMasta@users.noreply.github.com>
2023-02-10 21:38:05 +0100

    logout icon

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,6 +2,8 @@ import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/operations';
 import { useAuth } from 'hooks';
 import css from './UserMenu.module.scss';
+import IconButton from '@mui/material/IconButton';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
@@ -12,9 +14,10 @@ export const UserMenu = () => {
   return (
     <div className={css.wrapper}>
       <p className={css.username}>Welcome, {user.name}</p>
-      <button type="button" className={css.button} onClick={() => dispatch(logOut())}>
-        Logout
-      </button>
+
+      <IconButton title="Logout" onClick={() => dispatch(logOut())}>
+        <LogoutIcon />
+      </IconButton>
     </div>
   );
 };
