9bf8e1fea56d63970d86b433149c6b1df45bc618
Bolek <103572991+BoloMasta@users.noreply.github.com>
2023-02-12 15:04:41 +0100

    authnav navlink

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,18 +1,41 @@
 import { NavLink } from 'react-router-dom';
-import css from './AuthNav.module.scss';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
+const styles = {
+  activeLink: {
+    color: 'red',
+    display: 'inline-block',
+    textDecoration: 'underline',
+    padding: '12px',
+    fontWeight: '700',
+    fontSize: '1.3rem',
+  },
+  link: {
+    color: 'black',
+    display: 'inline-block',
+    textDecoration: 'none',
+    padding: '12px',
+    fontWeight: '700',
+    fontSize: '1.3rem',
+  },
+};
+
 export const AuthNav = () => (
-  <div className={css.wrapper}>
-    <NavLink className={css.link} to="/register">
+  <Box
+    sx={{
+      marginLeft: 'auto',
+    }}
+  >
+    <NavLink to="/register" style={({ isActive }) => (isActive ? styles.activeLink : styles.link)}>
       <Typography variant="h6" fontWeight="600">
-        Registration
+        Register
       </Typography>
     </NavLink>
-    <NavLink className={css.link} to="/login">
+    <NavLink to="/login" style={({ isActive }) => (isActive ? styles.activeLink : styles.link)}>
       <Typography variant="h6" fontWeight="600">
         Login
       </Typography>
     </NavLink>
-  </div>
+  </Box>
 );
